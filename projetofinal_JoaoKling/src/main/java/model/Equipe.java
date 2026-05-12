a48b0717c8e7394d8348efeec7daad97bb864317
joaokling <joaomarde@gmail.com>
2026-05-12 19:18:48 -0300

    Update Equipe.java

--- projetofinal_JoaoKling/src/main/java/model/Equipe.java
+++ projetofinal_JoaoKling/src/main/java/model/Equipe.java
@@ -1,18 +1,15 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Equipe {
 
     private long id;
-    private ArrayList<Bombeiro> bombeiros;
+    private Bombeiro bombeiro;
 
     public Equipe(long id) {
         this.id = id;
-        this.bombeiros = new ArrayList<>();
     }
 
-    public void adicionarBombeiro(Bombeiro b) {
-        bombeiros.add(b);
+    public void adicionarBombeiro(Bombeiro bombeiro) {
+        this.bombeiro = bombeiro;
     }
 }
