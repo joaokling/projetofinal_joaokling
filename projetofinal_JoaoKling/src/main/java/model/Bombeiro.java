31ada1a25d76c84227fa043accead97b5c52244d
joaokling <joaomarde@gmail.com>
2026-05-12 19:17:39 -0300

    Update Bombeiro.java

--- projetofinal_JoaoKling/src/main/java/model/Bombeiro.java
+++ projetofinal_JoaoKling/src/main/java/model/Bombeiro.java
@@ -1,15 +1,13 @@
 package model;
 
-public class Bombeiro extends Pessoa {
+public class Bombeiro {
 
     private String nomeGuerra;
     private String patente;
     private String matricula;
 
     public Bombeiro(long id, String nome, String contato,
-                     String nomeGuerra, String patente, String matricula) {
-
-        super(id, nome, contato);
+            String nomeGuerra, String patente, String matricula) {
 
         this.nomeGuerra = nomeGuerra;
         this.patente = patente;
