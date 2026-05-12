23c0c1ef89991026937fcde9f3ade3e69932f9b8
joaokling <joaomarde@gmail.com>
2026-05-12 19:19:29 -0300

    Update Ocorrencia.java

--- projetofinal_JoaoKling/src/main/java/model/Ocorrencia.java
+++ projetofinal_JoaoKling/src/main/java/model/Ocorrencia.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Ocorrencia {
 
     private long id;
@@ -12,31 +10,15 @@ public class Ocorrencia {
     private Usuario usuario;
     private Relatorio relatorio;
 
-    private ArrayList<Equipe> equipes;
-    private ArrayList<Viatura> viaturas;
-
-    public Ocorrencia(long id, String tipo, String local,
-                      String status, Usuario usuario) {
-
+    public Ocorrencia(long id, String tipo, String local, String status, Usuario usuario) {
         this.id = id;
         this.tipo = tipo;
         this.local = local;
         this.status = status;
         this.usuario = usuario;
-
-        equipes = new ArrayList<>();
-        viaturas = new ArrayList<>();
     }
 
-    public void adicionarEquipe(Equipe e) {
-        equipes.add(e);
-    }
-
-    public void adicionarViatura(Viatura v) {
-        viaturas.add(v);
-    }
-
-    public void finalizar(String descricaoRelatorio) {
-        this.relatorio = new Relatorio(1, descricaoRelatorio);
+    public void finalizar() {
+        status = "Finalizada";
     }
 }
